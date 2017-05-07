4a4346e18cb1423b56f40cf2cc1aa537d13c8987
ttallman <dspiteself@gmail.com>
2017-05-07 17:34:37 -0500

    bump the beam version

--- project.clj
+++ project.clj
@@ -6,7 +6,8 @@
   :source-paths ["src/clj"]
   :java-source-paths ["src/java"]
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.apache.beam/beam-sdks-java-core "0.3.0-incubating"]]
+                 [org.apache.beam/beam-sdks-java-core "0.6.0"]]
   :profiles {:dev {:source-paths ["dev"]}
              :provided
-             {:dependencies [[org.apache.beam/beam-runners-direct-java "0.3.0-incubating"]]}})
+             {:dependencies [[org.apache.beam/beam-runners-google-cloud-dataflow-java "0.6.0"]
+                             [org.apache.beam/beam-runners-direct-java "0.6.0"]]}})
